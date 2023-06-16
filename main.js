d2a67066b1ddf8d9b10fe932ad3ccc6c1271677b
Skyfe24 <fabio.erase@gmail.com>
2023-06-16 16:37:19 +0200

    Vue try #1

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 console.log("JS OK");
 
+console.log("Vue OK", Vue);
+
+
+// Così si costruisce il metodo createApp
+const { createApp } = Vue;
+
+// Inizializziamo l'app Vue
+
+const app = createApp({
+  data() {
+    return {
+      letterainiziale: 'ffffffffffffff',
+
+    }
+  },
+  
+})
+// La monto nel mio id chiamato 'prova'
+app.mount('prova');
